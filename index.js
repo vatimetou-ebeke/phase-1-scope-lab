bc1de6138d2bf68a55338e27a48dfd91f556f260
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-05 16:23:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName="bob"
+function  upperCaseCustomerName(){
+return customerName.toUpperCase();
+ 
+}
+
+  bestCustomer ="maybe bob";
+
+function setBestCustomer(){
+    
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    const bestCustomer="marry"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer="daniel"
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer="jack"
+}
