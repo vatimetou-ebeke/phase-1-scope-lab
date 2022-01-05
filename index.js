103ff878d854fe6a04938239086723d9f6969cae
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-05 16:47:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your solution in this file!
 var customerName="bob"
 function  upperCaseCustomerName(){
-return customerName.toUpperCase();
+return customerName.toUpperCase()
  
 }
 
-  bestCustomer ="maybe bob";
+ var bestCustomer ="maybe bob";
 
 function setBestCustomer(){
     
